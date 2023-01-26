3325b156b09d22d0093a749fc86c62beb5a07341
fytm <ftamakloe62@gmail.com>
2023-01-26 23:04:35 +0000

    Created a global advisor for handling custom errors

---
+++ src/main/java/com/hotelapp/demo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.hotelapp.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = HotelNotFoundException.class)
+    public ResponseEntity<String> catchException(HotelNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
