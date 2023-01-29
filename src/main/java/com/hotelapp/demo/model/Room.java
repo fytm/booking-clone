d19776eb7f108f31f20296a12faf6664e2265fea
fytm <ftamakloe62@gmail.com>
2023-01-29 16:35:08 +0000

    Changed structure of Room entity

--- src/main/java/com/hotelapp/demo/model/Room.java
+++ src/main/java/com/hotelapp/demo/model/Room.java
@@ -1,10 +1,10 @@
 package com.hotelapp.demo.model;
 
 import com.hotelapp.demo.enums.RoomStatus;
-import jakarta.annotation.Nonnull;
-import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -15,19 +15,17 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @Nonnull
+    @NonNull
     @Column(name = "number", nullable = false)
     String number;
-    @Nonnull
     @Column(name = "floor", nullable = false)
     int floor;
-    @Nonnull
     @Column(name = "capacity", nullable = false)
     int capacity;
-    @Nonnull
+    @NonNull
     @Column(name = "status", nullable = false)
     RoomStatus status;
-    @Nonnull
+    @NonNull
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "hotel.id", nullable = false)
     Hotel hotel;
