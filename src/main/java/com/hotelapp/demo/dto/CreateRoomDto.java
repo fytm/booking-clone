067c029528d76e4e4979590ac937c31116620772
fytm <ftamakloe62@gmail.com>
2023-02-06 23:09:36 +0000

    Added foreign key field for hotel uuid to map room to hotel

--- src/main/java/com/hotelapp/demo/dto/CreateRoomDto.java
+++ src/main/java/com/hotelapp/demo/dto/CreateRoomDto.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CreateRoomDTO {
+public class CreateRoomDto {
     String number;
     int floor;
     int capacity;
     RoomStatus status;
+    UUID hotelId;
 }
